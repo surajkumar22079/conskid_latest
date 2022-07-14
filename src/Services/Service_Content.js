2a0ff92267719b282049e34e57ac887c4fce8429
surajkumar22079 <90059548+surajkumar22079@users.noreply.github.com>
2022-07-14 21:57:12 +0530

    Update Service_Content.js
    Edit "14072022"

--- src/Services/Service_Content.js
+++ src/Services/Service_Content.js
@@ -5,35 +5,43 @@ const service_contents = [
     {
         Heading:"Internship Pool",
         Logo:"../Asset/Component 56 – 1.svg",
-        Description:"Discover your career journey accurately. Plan before you hustle!"
+        Description:"Discover your career journey accurately. Plan before you hustle!",
+        Link:"https://forms.gle/VKdpTjGXXDD1yJDg7"
     }, {
         Heading:"Resume Builder",
         Logo:"../Asset/Component 57 – 1.svg",
-        Description:"Professional resume, personalized layout"
+        Description:"Professional resume, personalized layout",
+        Link:"/Sign Up"
     }, {
         Heading:"Off-Campus Placements",
         Logo:"../Asset/Component 58 – 1.svg",
-        Description:"Get a high paying placement"
+        Description:"Get a high paying placement",
+        Link:"https://forms.gle/VKdpTjGXXDD1yJDg7"
     }, {
         Heading:"Campaigns, Updates",
         Logo:"../Asset/Component 59 – 1.svg",
-        Description:"Industrial advancements and notifications"
+        Description:"Industrial advancements and notifications",
+        Link:"/Sign Up"
     }, {
         Heading:"Career Counselling",
         Logo:"../Asset/Component 60 – 1.svg",
-        Description:"Exposure and interaction from industry professionals"
+        Description:"Exposure and interaction from industry professionals",
+        Link:"/Sign Up"
     }, {
         Heading:"Mock Interview",
         Logo:"../Asset/Interview-rafiki.svg",
-        Description:"Do’s & don’ts, clarify responses, stress handling"
+        Description:"Do’s & don’ts, clarify responses, stress handling",
+        Link:"/Sign Up"
     }, {
         Heading:"Certified Courses",
         Logo:"../Asset/Component 61 – 1.svg",
-        Description:"Relevant courses, valuable certification"
+        Description:"Relevant courses, valuable certification",
+        Link:"/Sign Up"
     }, {
         Heading:"Summer Training",
         Logo:"../Asset/Component 63 – 1.svg",
-        Description:"Live sessions, Skill-oriented training"
+        Description:"Live sessions, Skill-oriented training",
+        Link: "https://forms.gle/DAd9CBJqHhYukiDH6"
     }
 ]
 
